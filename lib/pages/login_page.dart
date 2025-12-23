39afde0beebbe9b3c3a1b52196c431e8ced82926
AgustinusGalihGumilang <agustinus9141@gmail.com>
2025-12-23 21:58:09 +0700

    Start page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,6 @@
 import 'package:dinacomapp/Components/custom_teks.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+
 
 class LoginPage extends StatelessWidget {
   LoginPage({super.key});
--- lib/pages/start_page.dart
+++ lib/pages/start_page.dart
@@ -1,5 +1,4 @@
 import 'package:dinacomapp/Components/custom_button.dart';
-import 'package:dinacomapp/Components/custom_color.dart';
 import 'package:flutter/material.dart';
 
 
