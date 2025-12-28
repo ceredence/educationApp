f4121d83d4a3c50d6ea509da7b7134ea6190215b
AgustinusGalihGumilang <agustinus9141@gmail.com>
2025-12-29 00:48:08 +0700

    [FE][AGG]membenarkan route

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -2,4 +2,7 @@ class AppRoutes {
   static const String testingPage = '/testingPage';
   static const String startPage = '/startPage';
   static const String homePage = '/homePage';
+  static const String levelPage = '/levelPage';
+  static const String categoryPage = '/categoryPage';
+  static const String taskPage = '/taskPage';
 }
