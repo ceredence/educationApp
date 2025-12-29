69acce2c1b2082aeb869574ef77c84dd48949e37
AgustinusGalihGumilang <agustinus9141@gmail.com>
2025-12-30 01:03:10 +0700

    [FE][AGG]Menambahkan Percent Indicator

--- lib/Components/custom_color.dart
+++ lib/Components/custom_color.dart
@@ -6,4 +6,6 @@ import 'package:flutter/material.dart';
   static const Color button   = Color(0xFFFBFBFB);
   static const Color button2   = Color(0xFFD9D9D9);
   static const Color tekscolor = Color(0xFF696969);
+  static const Color btnhomebiru = Color(0xFFCDE3F8);
+  static const Color oubtnhomebiru = Color(0xFF92C0E9);
 }
