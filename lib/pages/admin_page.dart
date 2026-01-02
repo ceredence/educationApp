2346ca1c93b255ad59a17a82b03501bf2c50e7dd
robotganteng09 <arsyafaisyar@gmail.com>
2026-01-02 17:54:57 +0700

    buat page Admin [AMF]

---
+++ lib/pages/admin_page.dart
@@ -0,0 +1,147 @@
+import 'package:dinacomapp/Components/admin_textfield.dart';
+import 'package:dinacomapp/Components/custom.textfild.dart';
+import 'package:dinacomapp/Components/custom_button.dart';
+import 'package:dinacomapp/Components/custom_dropdown.dart';
+import 'package:flutter/material.dart';
+
+class AdminPage extends StatefulWidget {
+  const AdminPage({super.key});
+
+  @override
+  State<AdminPage> createState() => _AdminPageState();
+}
+
+class _AdminPageState extends State<AdminPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  String question = "";
+  String optionA = "";
+  String optionB = "";
+  String optionC = "";
+
+  String? correctAnswer;
+  String? level;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Admin - Tambah Soal"),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              /// Pertanyaan
+              AdminTextField(
+                hintText: "isi pertanyaan",
+                onChanged: (value) {},
+                buttonText: "Tambahkan gambar",
+                onButtonPressed: () {
+                  print("Button diklik");
+                },
+              ),
+              const SizedBox(height: 16),
+
+              /// Jawaban
+              AdminTextField(
+                hintText: "jawaban A",
+                onChanged: (value) {},
+                buttonText: "Tambahkan gambar",
+                onButtonPressed: () {
+                  print("Button diklik");
+                },
+              ),
+              const SizedBox(height: 10),
+              AdminTextField(
+                hintText: "jawaban B",
+                onChanged: (value) {},
+                buttonText: "Tambahkan gambar",
+                onButtonPressed: () {
+                  print("Button diklik");
+                },
+              ),
+              const SizedBox(height: 10),
+              AdminTextField(
+                hintText: "jawaban C",
+                onChanged: (value) {},
+                buttonText: "Tambahkan gambar",
+                onButtonPressed: () {
+                  print("Button diklik");
+                },
+              ),
+
+              const SizedBox(height: 16),
+
+              /// Jawaban yang benar
+              const Text(
+                "Jawaban Benar",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              CustomDropdown<String>(
+                hintText: "pilih jawaban yang benar",
+                value: correctAnswer,
+                items: const [
+                  DropdownMenuItem(value: "A", child: Text("A")),
+                  DropdownMenuItem(value: "B", child: Text("B")),
+                  DropdownMenuItem(value: "C", child: Text("C")),
+                ],
+                onChanged: (value) {
+                  setState(() {
+                    correctAnswer = value!;
+                  });
+                },
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                "Level Soal",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+
+              CustomDropdown<String>(
+                hintText: "pilih level",
+                value: level,
+                items: const [
+                  DropdownMenuItem(value: "low", child: Text("Mudah")),
+                  DropdownMenuItem(value: "medium", child: Text("Sedang")),
+                  DropdownMenuItem(value: "high", child: Text("Sulit")),
+                ],
+                onChanged: (value) {
+                  setState(() {
+                    level = value!;
+                  });
+                },
+              ),
+
+              const SizedBox(height: 16),
+
+              /// Level Soal
+              const SizedBox(height: 24),
+
+              /// Tombol Simpan
+              CustomButton(
+                text: "Simpan",
+                onPressed: () {
+                  final data = {
+                    "question": question,
+                    "A": optionA,
+                    "B": optionB,
+                    "C": optionC,
+                    "answer": correctAnswer,
+                    "level": level,
+                  };
+
+                  print(data);
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
